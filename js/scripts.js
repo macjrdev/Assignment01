684bf26abf793fb6e69037001c17f5292ea9e6ce
macjrdev <macjrdev824@gmail.com>
2022-02-21 23:04:42 -0800

    Assignment 1 complete (Excluding Steps 10 & 16)

---
+++ js/scripts.js
@@ -0,0 +1,117 @@
+//STEP 1
+// let someMonth;
+// function theMonth ();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+//STEP 2
+// 824                 //numeric literal
+// `This is a string`  //string literal
+// true                //Boolean literal
+// null                //null literal
+
+//STEP 3
+// var anExample = 8 + 2(4/5) - 9;
+// var aSecondExample = anExample + (1/4) + 8;
+
+//STEP 4
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+//STEP 5
+// let firstName;
+// firstName = 'Martin';
+// 
+// let city = 'San Diego'
+// 
+// let lastName = 'McAllister', yourAge = '34';
+
+//STEP 6
+// let yourName = 'Martin McAllister Jr'
+// let age = '34'
+// console.log(yourName + ' is ' + age + ' years old ');
+
+//STEP 7
+// var month = "Feburary"
+// console.log( month + " is the second Month of the Year " + Boolean("Feburary"));
+// 
+// let x = 0;
+// Boolean(x);
+// console.log(Boolean(x))
+
+//STEP 8
+// let someString = 'Who once said, \\"Only two things are infinite, the universe and human stupidity, and Im not sure about the former."\\';
+// 
+// window.console.log(someString);
+
+//STEP 9
+// let x = 1
+// x = null
+// console.log(x);
+// 
+// let y;
+// console.log(y);
+
+//STEP 10
+
+
+
+
+//STEP 11
+// let myName = 'Martin McAllister Jr.'
+// alert(' Hello ' + myName + ', welcome to the JavaScript class! ');
+
+//STEP 12
+// var Name = 'Martin'
+// alert(' Hello ' + Name + ', welcome to the JavaScript class! ');
+
+//STEP 13
+// var course = 'JavaScript'
+// alert(course);
+
+//STEP 14
+//alert('Hello Martin McAllister Jr.\nWelcome to the JavaScript class!')
+
+//STEP 15
+// let yourName = prompt('What is your Name?');
+// console.log(yourName);
+
+//STEP 16;
+
+
+
+
+//STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y);
+
+//STEP 18
+// let x = 20
+// console.log(x + 20);
+
+//STEP 19
+// let x = 20
+// console.log(x * 5);
+
+//STEP 20
+// let x = 20 / 3
+// console.log(x / 1);
+
+//STEP 21
+// let x = 5;
+// let y = 5;
+// console.log(x === y);
+
+//STEP 22
+// let x = 4
+// let y = 10
+// console.log(x >= y);
